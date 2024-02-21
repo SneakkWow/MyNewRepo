fb90aeca09038b2b36e05c0a38d58226b424fc42
SneakkWow <logan.c.turney@outlook.com>
2024-02-20 20:10:33 -0600

    Added Basically Everything

---
+++ CGEPrototype1/Assets/TriggerZone.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TriggerZone : MonoBehaviour
+{
+
+    public TMP_Text output;
+    public string textToDisplay;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            //Debug.Log("Triggered by " + collision.gameObject.tag);
+            output.text = textToDisplay;
+        }        
+    }
+}
