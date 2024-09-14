7c8050cc65e0af686c5e39c22868718ff0e1cc86
SneakkWow <logan.c.turney@outlook.com>
2024-09-14 17:01:29 -0500

    Prototype 2 Finished

---
+++ 401Prototype2/Assets/Scripts/DestroyOutOfBounds.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+* Logan Turney
+* 401Prototype2
+* Assignment 3
+* Destroys game objects when they go too far out of bounds
+*/
+public class DestroyOutOfBounds : MonoBehaviour
+{
+    public float topBound = 45;
+    public float bottomBound = -10;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.z > topBound)
+        {
+            Destroy(gameObject);
+        }
+
+        if (transform.position.z < bottomBound)
+        {
+            GameObject.FindGameObjectWithTag("HealthSystem").GetComponent<HealthSystem>().TakeDamage();
+
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ 401Prototype2/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+* Logan Turney
+* 401Prototype2
+* Assignment 3
+* Spawns the animals at the top of the screen in random intervals and random placements
+*/
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] prefabsToSpawn;
+
+    public HealthSystem healthSystem;
+
+    private float leftBound = -15;
+    private float rightBound = 15;
+    private float spawnPosZ = 40;
+
+    public bool gameOver = false;
+
+    void Start()
+    {
+        //InvokeRepeating("SpawnRandomPrefab", 2, 1.5f);
+
+        StartCoroutine(SpawnRandomPrefabWithCoroutine());
+
+        healthSystem = GameObject.FindGameObjectWithTag("HealthSystem").GetComponent<HealthSystem>();
+    }
+
+    IEnumerator SpawnRandomPrefabWithCoroutine()
+    {
+        yield return new WaitForSeconds(3f);
+
+        while (!healthSystem.gameOver)
+        {
+            SpawnRandomPrefab();
+
+            float randomDelay = Random.Range(1.5f, 3.0f);
+
+            yield return new WaitForSeconds(randomDelay);
+        }
+    }
+
+    // Update is called once per frame
+    /*void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            SpawnRandomPrefab();
+        }
+
+    }*/
+
+    void SpawnRandomPrefab()
+    {
+        int prefabIndex = Random.Range(0, prefabsToSpawn.Length);
+
+        Vector3 spawnPos = new Vector3(Random.Range(leftBound, rightBound), 0, spawnPosZ);
+
+        Instantiate(prefabsToSpawn[prefabIndex], spawnPos, prefabsToSpawn[prefabIndex].transform.rotation);
+    }
+}
