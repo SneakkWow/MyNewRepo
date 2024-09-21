48dc4e6843b58f0d1b703cb5b9bc245176c1b9e3
SneakkWow <logan.c.turney@outlook.com>
2024-09-21 15:43:41 -0500

    prototype3

---
+++ 401Prototype3/Assets/Scripts/MoveLeft.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLeft : MonoBehaviour
+{
+    public float speed = 30f;
+
+    private PlayerController playerControllerScript;
+
+    private float leftBound = -15;
+
+    void Start()
+    {
+        playerControllerScript = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (playerControllerScript.gameOver == false)
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+        
+
+        //destroy obstacle out of bounds
+        if (transform.position.x < leftBound && gameObject.CompareTag("Obstacle"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
